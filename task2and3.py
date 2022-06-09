186cc4bc02c98e78da3c50eef476412fca2a28b1
esentur <esenturdildebekov8@gmail.com>
2022-06-09 20:31:38 +0600

    task2 and task3

---
+++ task2and3.py
@@ -0,0 +1,8 @@
+print('Task2: git help')
+print('Task3:\n',
+      'mkdir test\n',
+      'touch test\main.py\n',
+      'nano test\main.py\n',
+      'def hello():print("Hello World!")\n',
+      'cd test\n',
+      'git init\')')
